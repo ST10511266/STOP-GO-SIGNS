3ee333bf11289fa94b281d11d8d86e346edcf1eb
Leagilwe Mapoka <ST10511266@myemeris.edu.za>
2026-02-24 12:53:37 +0200

    First Submission

---
+++ src/stop/gosigns/StopGoSigns.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package stop.gosigns;
+
+/**
+ *
+ * @author mapok
+ */
+public class StopGoSigns {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("      **********                                    +");
+        
+        System.out.println("    *             *                             +       +");
+        
+        System.out.println("  *     S T O P     *                        +      G O      +");
+        
+        System.out.println("    *              *                      +                      +  ");
+        
+        System.out.println("      **********                        ++++++++++++++++++++++++++++");
+        
+        System.out.println("          **                                       +++");
+        
+        System.out.println("          **                                       +++");
+        
+        System.out.println("          **                                       +++");
+        
+        System.out.println("          **                                       +++");
+        
+        System.out.println("          **                                       +++");
+        
+        System.out.println("         ****                                    +++++++  ");
+        
+        System.out.println("     ************                             +++++++++++++");
+        System.out.println("|------------------------------------------------------------------------|");
+        System.out.println("|------------------------------------------------------------------------|");
+    }
+    
+}
